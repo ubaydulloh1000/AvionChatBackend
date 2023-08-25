5878d758702bc836cdd9d07a44e7fd9a3ca8d883
Ubaydulloh <ubaydulloh1000@mail.ru>
2023-08-25 16:34:54 +0500

    FEAT: INIT

--- core/asgi.py
+++ core/asgi.py
@@ -2,6 +2,11 @@ import environ
 
 from django.core.asgi import get_asgi_application
 
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -11,15 +16,8 @@ from apps.chat import routing as chat_routing
 env = environ.Env()
 environ.Env.read_env(".env")
 
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
-
-
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
-
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(URLRouter(chat_routing.websocket_urlpatterns))
     )
