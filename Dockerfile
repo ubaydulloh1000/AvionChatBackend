1c630450278ef4ad27e4dfadf5de7f7e4d912c59
Ubaydulloh Kuchkarov <ubaydulloh1000@gmail.com>
2023-09-04 16:57:08 +0500

    FIX: daphne docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,16 @@
-FROM python:3.8-slim-buster
+FROM python:3.10-alpine
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
 ENV HOME_DIR=/app
-#RUN mkdir -p $HOME_DIR
 
 WORKDIR $HOME_DIR
 
-COPY requirements requirements
+COPY pyproject.toml poetry.lock ./
 
-RUN pip install --upgrade pip
-RUN pip install -r requirements/production.txt
+RUN pip install poetry && poetry install --no-root && poetry add gunicorn
 
+COPY . /app
 
-RUN useradd app && usermod -aG app app
-
-COPY . .
-
-RUN chown -R app:app $HOME_DIR
-
-USER app
 RUN chmod a+x entrypoint.sh
-
-#ENTRYPOINT ./entrypoint.sh
--- core/asgi.py
+++ core/asgi.py
@@ -4,17 +4,16 @@ from django.core.asgi import get_asgi_application
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
+env = environ.Env()
+environ.Env.read_env(".env")
 django_asgi_app = get_asgi_application()
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
 from apps.chat import routing as chat_routing
 
-env = environ.Env()
-environ.Env.read_env(".env")
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
