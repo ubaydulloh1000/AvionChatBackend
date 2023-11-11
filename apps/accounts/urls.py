1164b5f7cf7f3584b4c66a6564c416e18fed679e
Ubaydulloh <ubaydulloh1000@mail.ru>
2023-11-11 17:16:00 +0500

    test: cache page decorator

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.decorators.cache import cache_page
 from . import views
 
 app_name = 'accounts'
+
+FIVE_MINUTES = 60 * 10
+ONE_HOUR = 60 * 60
+TWO_HOUR = ONE_HOUR * 2
 
 urlpatterns = [
     path(
@@ -27,7 +32,7 @@ urlpatterns = [
     ),
     path(
         "me/",
-        views.AccountDetailAPIView.as_view(),
+        cache_page(ONE_HOUR)(views.AccountDetailAPIView.as_view()),
         name="account_detail",
     ),
     path(
@@ -37,12 +42,12 @@ urlpatterns = [
     ),
     path(
         "list/",
-        views.UserListAPIView.as_view(),
+        cache_page(TWO_HOUR)(views.UserListAPIView.as_view()),
         name="user_list",
     ),
     path(
         "profile/<int:pk>/",
-        views.UserProfileAPIView.as_view(),
+        cache_page(FIVE_MINUTES)(views.UserProfileAPIView.as_view()),
         name="user_profile",
     ),
 ]
