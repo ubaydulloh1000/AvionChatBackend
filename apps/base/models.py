f4da895ab01837d47bcb6a6090c396d7af1d35b1
Ubaydulloh <ubaydulloh1000@mail.ru>
2023-11-20 20:33:23 +0500

    FEAT: is_deleted field added to base model

--- apps/base/models.py
+++ apps/base/models.py
@@ -8,3 +8,5 @@ class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Yaratilgan sana"))
     updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Yangilangan sana"))
+    is_deleted = models.BooleanField(verbose_name=_("Is deleted?"), default=False)
+    deleted_at = models.DateTimeField(verbose_name=_("deleted at"), null=True, blank=True)
